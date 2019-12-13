19483b05e05b271d2831e701351fb0759d4fcb11
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-12-14 01:12:09 +0530

    Fill Lume Authentication app

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use  App\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        // return $request;
+        //validate incoming request
+        $this->validate($request, [
+            'firstName' => 'required|string',
+            'lastName' => 'required|string',
+            'username' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        try {
+            $user = new User;
+            $user->email = $request->input('email');
+            $plainPassword = $request->input('password');
+            $user->username = $request->input('username');
+            $user->lastName = $request->input('lastName');
+            $user->firstName = $request->input('firstName');
+            $user->password = app('hash')->make($plainPassword);
+            $user->save();
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'CREATED'], 201);
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => 'User Registration Failed!'], 409);
+        }
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function login(Request $request)
+    {
+        //validate incoming request
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        // return response()->json($credentials);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    // Global Authentication function
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ], 200);
+    }
+}
